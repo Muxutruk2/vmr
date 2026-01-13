ab7e718c6bf7e388078d8f8e76f96961aa033844
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2026-01-13 18:44:55 +0100

    Separate main and lib and misc changes

--- src/bin/runner.rs
+++ src/bin/runner.rs
@@ -1,221 +1,22 @@
-use std::{fmt::Display, path::Path, usize};
-#[macro_use]
-extern crate num_derive;
-use bytemuck::Pod;
+use clap::Parser;
+use libvmr::{Operation, Register};
 use log::{debug, error};
-use num_traits::FromPrimitive;
-
-#[repr(usize)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, FromPrimitive)]
-pub enum Register {
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
-    R7,
-    R8,
-    R9,
-    R10,
-    R11,
-    R12,
-    R13,
-    R14,
-    R15,
-    R16,
-    RSP,
-}
+use num_traits::cast::FromPrimitive;
+use std::path::PathBuf;
 
 pub type Immediate = u16;
 pub type Offset = i16;
 
-#[repr(u8)]
-#[allow(non_camel_case_types)]
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, FromPrimitive)]
-enum Operation {
-    // Misc [0]
-    HALT = 0x00,
-    NOP = 0x01,
-
-    // Move [1]
-    MOV = 0x10,     // R, R
-    MOV_IMM = 0x11, // R, IMM
-
-    // Load/Store [2-3]
-    // Load: Destination is always a Register
-    LOAD = 0x20,     // [R], R
-    LOAD_REL = 0x21, // [R + OFFSET], R
-    LOAD_IMM = 0x22, // [IMM], R
-
-    // Store: Register Source
-    STORE_R_R = 0x30,   // [R], R
-    STORE_REL_R = 0x31, // [R + OFFSET], R
-    STORE_IMM_R = 0x32, // [IMM], R
-
-    // Store: Immediate Source
-    STORE_R_IMM = 0x33,   // [R], IMM
-    STORE_REL_IMM = 0x34, // [R + OFFSET], IMM
-    STORE_IMM_IMM = 0x35, // [IMM], IMM
-
-    // Arithmetic & Logic [4-6]
-    ADD = 0x40,     // R, R
-    ADD_IMM = 0x41, // R, IMM
-    SUB = 0x42,     // R, R
-    SUB_IMM = 0x43, // R, IMM
-
-    AND = 0x50,     // R, R
-    AND_IMM = 0x51, // R, IMM
-    OR = 0x52,      // R, R
-    OR_IMM = 0x53,  // R, IMM
-    XOR = 0x54,     // R, R
-    XOR_IMM = 0x55, // R, IMM
-
-    // Shifts [7]
-    SHL = 0x70,     // R, R
-    SHL_IMM = 0x71, // R, IMM
-    SHR = 0x72,     // R, R
-    SHR_IMM = 0x73, // R, IMM
-
-    // Comparison [8]
-    CMP = 0x80,     // R, R
-    CMP_IMM = 0x81, // R, IMM
-
-    // Stack Operations [9-A]
-    PUSH = 0x90,     // R
-    PUSH_M = 0x91,   // [R]
-    PUSH_IMM = 0x92, // IMM
-
-    POP = 0xA0,     // R
-    POP_M = 0xA1,   // [R]
-    POP_IMM = 0xA2, // IMM -- Pop into specific address
-
-    // Control Flow [B-D]
-    // Jumps (Address from Register or Immediate)
-    JMP = 0xB0,
-    JMP_IMM = 0xB1,
-
-    // Conditionals: Zero / Equal
-    JE = 0xB2,
-    JE_IMM = 0xB3,
-
-    // Conditionals: Not Zero / Not Equal
-    JNE = 0xB6,
-    JNE_IMM = 0xB7,
-
-    // Subroutines
-    CALL = 0xD0,     // R
-    CALL_IMM = 0xD1, // IMM
-    RET = 0xD2,      // ()
-}
-
-impl Display for Operation {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl Operation {
-    #[allow(dead_code)]
-    pub fn arg_type(&self) -> Arguments {
-        match self {
-            // Misc: 0 Operands
-            Operation::HALT | Operation::NOP | Operation::RET => Arguments::None,
-
-            // Single Operand: Register or Immediate
-            Operation::PUSH
-            | Operation::PUSH_M
-            | Operation::PUSH_IMM
-            | Operation::POP
-            | Operation::POP_M
-            | Operation::POP_IMM
-            | Operation::JMP
-            | Operation::JMP_IMM
-            | Operation::JE
-            | Operation::JE_IMM
-            | Operation::JNE
-            | Operation::JNE_IMM
-            | Operation::CALL
-            | Operation::CALL_IMM => Arguments::Reg,
-
-            // Two Operands: Register, Register
-            Operation::MOV
-            | Operation::LOAD
-            | Operation::STORE_R_R
-            | Operation::ADD
-            | Operation::SUB
-            | Operation::AND
-            | Operation::OR
-            | Operation::XOR
-            | Operation::SHL
-            | Operation::SHR
-            | Operation::CMP => Arguments::RegReg,
-
-            // Two Operands: Register, Immediate
-            Operation::MOV_IMM
-            | Operation::STORE_R_IMM
-            | Operation::ADD_IMM
-            | Operation::SUB_IMM
-            | Operation::AND_IMM
-            | Operation::OR_IMM
-            | Operation::XOR_IMM
-            | Operation::SHL_IMM
-            | Operation::SHR_IMM
-            | Operation::CMP_IMM => Arguments::RegImm,
-
-            // Two Operands: Immediate, Register
-            Operation::LOAD_IMM | Operation::STORE_IMM_R => Arguments::ImmReg,
-
-            // Two Operands: Immediate, Immediate
-            Operation::STORE_IMM_IMM => Arguments::ImmImm,
-
-            // Three Operands: Register, Immediate, Register (Relative Addressing)
-            Operation::LOAD_REL | Operation::STORE_REL_R | Operation::STORE_REL_IMM => {
-                Arguments::RegImmReg
-            }
-        }
-    }
-}
-
-#[repr(u8)]
-#[allow(non_camel_case_types)]
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Arguments {
-    None = 0x00,
-    Reg = 0x01,
-    RegReg = 0x02,
-    RegImm = 0x03,
-    ImmImm = 0x04,
-    RegImmReg = 0x05,
-    ImmReg = 0x06,
-}
-
-impl Arguments {
-    fn to_offset(&self) -> u8 {
-        match self {
-            Arguments::None => 1,
-            Arguments::Reg => 2,
-            Arguments::RegReg => 3,
-            Arguments::RegImm => 4,
-            Arguments::ImmImm => 5,
-            Arguments::RegImmReg => 5,
-            Arguments::ImmReg => 4,
-        }
-    }
-}
-
 #[derive(Debug)]
-struct VirtualMachine<'a> {
+struct VirtualMachine {
     code: Vec<u8>,
     memory: Vec<u16>,
     equal: bool,
     current_instruction: u16,
-    registers: &'a mut [u16; 17],
+    registers: Vec<u16>,
 }
 
-impl<'a> std::fmt::Display for VirtualMachine<'a> {
+impl std::fmt::Display for VirtualMachine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "CODE\n")?;
         for byte in self.code.iter() {
@@ -267,13 +68,17 @@ pub enum RuntimeError {
     StackOverflow,
 }
 
-impl<'a> VirtualMachine<'a> {
+impl VirtualMachine {
     fn next_reg(&self, offset: u16) -> Result<usize, RuntimeError> {
         let reg_byte = self.next(offset)?;
         Ok(Register::from_u8(reg_byte).ok_or(RuntimeError::InvalidRegister)? as usize)
     }
 
-    fn next<T: Pod>(&self, offset: u16) -> Result<T, RuntimeError> {
+    fn next<T>(&self, offset: u16) -> Result<T, RuntimeError>
+    where
+        T: num_traits::FromBytes,
+        T::Bytes: for<'a> TryFrom<&'a [u8]>,
+    {
         let start_idx = (self.current_instruction as usize)
             .checked_add(offset as usize)
             .ok_or(RuntimeError::InstructionOOB)?;
@@ -287,7 +92,9 @@ impl<'a> VirtualMachine<'a> {
             .get(start_idx..end_idx)
             .ok_or(RuntimeError::InstructionOOB)?;
 
-        Ok(bytemuck::pod_read_unaligned(bytes))
+        let array: T::Bytes = bytes.try_into().map_err(|_| RuntimeError::InstructionOOB)?;
+
+        Ok(T::from_be_bytes(&array))
     }
 
     pub fn cycle(&mut self) -> Result<(), RuntimeError> {
@@ -483,7 +290,6 @@ impl<'a> VirtualMachine<'a> {
             }
             Operation::AND_IMM => {
                 let r1 = self.next_reg(1)?;
-
                 let imm2: u16 = self.next(2)?;
                 self.registers[r1] &= imm2;
                 None
@@ -661,18 +467,18 @@ impl<'a> VirtualMachine<'a> {
             Operation::RET => todo!(),
         };
 
-        // 3. Increment PC (Advance to the NEXT instruction)
-        let offset = arg_type.to_offset() as u16;
-        self.current_instruction += offset;
-
         if let Some(addr) = new_addr {
+            // If JUMP instruction called
             self.current_instruction = addr;
+        } else {
+            let offset = arg_type.to_offset() as u16;
+            self.current_instruction += offset;
         }
 
         Ok(())
     }
 
-    pub fn from_bytes(code: Vec<u8>, registers: &'a mut [u16; 17]) -> Self {
+    pub fn from_bytes(code: Vec<u8>, registers: Vec<u16>) -> Self {
         VirtualMachine {
             code: code.into_iter().skip(3).collect(),
             equal: false,
@@ -683,23 +489,29 @@ impl<'a> VirtualMachine<'a> {
     }
 }
 
+#[derive(clap::Parser)]
+struct Args {
+    input_file: PathBuf,
+}
+
 fn main() {
     env_logger::builder()
         .format_timestamp(None)
         .parse_default_env()
         .init();
 
-    let a: &Path = Path::new("test.vmr");
-    let code: Vec<u8> = std::fs::read(a).unwrap();
+    let args = Args::parse();
+
+    let code: Vec<u8> = std::fs::read(args.input_file).unwrap();
     let mut registers: Vec<u16> = Vec::with_capacity(17);
 
     for _ in 0..=17 {
         registers.push(0);
     }
+
     registers[Register::RSP as usize] = 0xFFFF;
 
-    let mut registers_slice: &mut [u16; 17] = &mut (registers[0..17].try_into().unwrap());
-    let mut vm = VirtualMachine::from_bytes(code, &mut registers_slice);
+    let mut vm = VirtualMachine::from_bytes(code, registers);
     loop {
         match vm.cycle() {
             Ok(_) => {}
---
+++ src/lib.rs
@@ -0,0 +1,197 @@
+#[macro_use]
+extern crate num_derive;
+
+#[repr(usize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, FromPrimitive)]
+pub enum Register {
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    R8,
+    R9,
+    R10,
+    R11,
+    R12,
+    R13,
+    R14,
+    R15,
+    R16,
+    RSP,
+}
+
+pub type Immediate = u16;
+pub type Offset = i16;
+
+#[repr(u8)]
+#[allow(non_camel_case_types)]
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, FromPrimitive)]
+pub enum Operation {
+    // Misc [0]
+    HALT = 0x00,
+    NOP = 0x01,
+
+    // Move [1]
+    MOV = 0x10,     // R, R
+    MOV_IMM = 0x11, // R, IMM
+
+    // Load/Store [2-3]
+    // Load: Destination is always a Register
+    LOAD = 0x20,     // [R], R
+    LOAD_REL = 0x21, // [R + OFFSET], R
+    LOAD_IMM = 0x22, // [IMM], R
+
+    // Store: Register Source
+    STORE_R_R = 0x30,   // [R], R
+    STORE_REL_R = 0x31, // [R + OFFSET], R
+    STORE_IMM_R = 0x32, // [IMM], R
+
+    // Store: Immediate Source
+    STORE_R_IMM = 0x33,   // [R], IMM
+    STORE_REL_IMM = 0x34, // [R + OFFSET], IMM
+    STORE_IMM_IMM = 0x35, // [IMM], IMM
+
+    // Arithmetic & Logic [4-6]
+    ADD = 0x40,     // R, R
+    ADD_IMM = 0x41, // R, IMM
+    SUB = 0x42,     // R, R
+    SUB_IMM = 0x43, // R, IMM
+
+    AND = 0x50,     // R, R
+    AND_IMM = 0x51, // R, IMM
+    OR = 0x52,      // R, R
+    OR_IMM = 0x53,  // R, IMM
+    XOR = 0x54,     // R, R
+    XOR_IMM = 0x55, // R, IMM
+
+    // Shifts [7]
+    SHL = 0x70,     // R, R
+    SHL_IMM = 0x71, // R, IMM
+    SHR = 0x72,     // R, R
+    SHR_IMM = 0x73, // R, IMM
+
+    // Comparison [8]
+    CMP = 0x80,     // R, R
+    CMP_IMM = 0x81, // R, IMM
+
+    // Stack Operations [9-A]
+    PUSH = 0x90,     // R
+    PUSH_M = 0x91,   // [R]
+    PUSH_IMM = 0x92, // IMM
+
+    POP = 0xA0,     // R
+    POP_M = 0xA1,   // [R]
+    POP_IMM = 0xA2, // IMM -- Pop into specific address
+
+    // Control Flow [B-D]
+    // Jumps (Address from Register or Immediate)
+    JMP = 0xB0,
+    JMP_IMM = 0xB1,
+
+    // Conditionals: Zero / Equal
+    JE = 0xB2,
+    JE_IMM = 0xB3,
+
+    // Conditionals: Not Zero / Not Equal
+    JNE = 0xB6,
+    JNE_IMM = 0xB7,
+
+    // Subroutines
+    CALL = 0xD0,     // R
+    CALL_IMM = 0xD1, // IMM
+    RET = 0xD2,      // ()
+}
+
+impl Operation {
+    #[allow(dead_code)]
+    pub fn arg_type(&self) -> Arguments {
+        match self {
+            // Misc: 0 Operands
+            Operation::HALT | Operation::NOP | Operation::RET => Arguments::None,
+
+            // Single Operand: Register or Immediate
+            Operation::PUSH
+            | Operation::PUSH_M
+            | Operation::PUSH_IMM
+            | Operation::POP
+            | Operation::POP_M
+            | Operation::POP_IMM
+            | Operation::JMP
+            | Operation::JMP_IMM
+            | Operation::JE
+            | Operation::JE_IMM
+            | Operation::JNE
+            | Operation::JNE_IMM
+            | Operation::CALL
+            | Operation::CALL_IMM => Arguments::Reg,
+
+            // Two Operands: Register, Register
+            Operation::MOV
+            | Operation::LOAD
+            | Operation::STORE_R_R
+            | Operation::ADD
+            | Operation::SUB
+            | Operation::AND
+            | Operation::OR
+            | Operation::XOR
+            | Operation::SHL
+            | Operation::SHR
+            | Operation::CMP => Arguments::RegReg,
+
+            // Two Operands: Register, Immediate
+            Operation::MOV_IMM
+            | Operation::STORE_R_IMM
+            | Operation::ADD_IMM
+            | Operation::SUB_IMM
+            | Operation::AND_IMM
+            | Operation::OR_IMM
+            | Operation::XOR_IMM
+            | Operation::SHL_IMM
+            | Operation::SHR_IMM
+            | Operation::CMP_IMM => Arguments::RegImm,
+
+            // Two Operands: Immediate, Register
+            Operation::LOAD_IMM | Operation::STORE_IMM_R => Arguments::ImmReg,
+
+            // Two Operands: Immediate, Immediate
+            Operation::STORE_IMM_IMM => Arguments::ImmImm,
+
+            // Three Operands: Register, Immediate, Register (Relative Addressing)
+            Operation::LOAD_REL | Operation::STORE_REL_R | Operation::STORE_REL_IMM => {
+                Arguments::RegImmReg
+            }
+        }
+    }
+}
+
+#[repr(u8)]
+#[allow(non_camel_case_types)]
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Arguments {
+    None = 0x00,
+    Reg = 0x01,
+    RegReg = 0x02,
+    RegImm = 0x03,
+    ImmImm = 0x04,
+    RegImmReg = 0x05,
+    ImmReg = 0x06,
+}
+
+impl Arguments {
+    pub fn to_offset(&self) -> u8 {
+        match self {
+            Arguments::None => 1,
+            Arguments::Reg => 2,
+            Arguments::RegReg => 3,
+            Arguments::RegImm => 4,
+            Arguments::ImmImm => 5,
+            Arguments::RegImmReg => 5,
+            Arguments::ImmReg => 4,
+        }
+    }
+}
