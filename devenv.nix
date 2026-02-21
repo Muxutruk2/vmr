cd49434de315fac3dc4b97969c329c5ea4eca463
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2026-02-21 18:54:48 +0100

    Add git hook for clippy and formatting

--- devenv.nix
+++ devenv.nix
@@ -11,4 +11,17 @@
   ];
 
   languages.rust.enable = true;
+
+  git-hooks.hooks = {
+    clippy = {
+      enable = true;
+      settings = {
+        allFeatures = true;
+        denyWarnings = true;
+      };
+    };
+    rustfmt = {
+      enable = true;
+    };
+  };
 }
