4232c10f4d94cd0197faad9b918fe838c52a54e6
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2026-03-03 20:54:07 +0100

    Add linker environment and also better Logs

--- devenv.nix
+++ devenv.nix
@@ -5,8 +5,11 @@
 }:
 
 {
+  env."VMR_LINKER" = "${config.env.DEVENV_ROOT}/target/release/linker";
   env."VMR_ASSEMBLER" = "${config.env.DEVENV_ROOT}/target/release/assembler";
   env."VMR_RUNNER" = "${config.env.DEVENV_ROOT}/target/release/runner";
+
+  env."RUST_LOG" = "info";
 
   packages = with pkgs; [
     hexedit
